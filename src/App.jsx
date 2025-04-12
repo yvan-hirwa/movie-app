e9d2146905b45ef33b9ace9929edb10048a5154a
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-04-13 01:24:13 +0200

    Movie-app init.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import { useState, useEffect } from 'react';
-
-const Card = ({ title  }) =>{
-const [count, setCount] = useState(0);
-const [hasLiked, setHasLiked] = useState(false);
-
-useEffect(() => {
-  console.log(`You have ${hasLiked ? 'liked' : 'not liked'} ${title}`);
-},[hasLiked]);
-
-
-  return (
-    <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
-      <h2>{title} <br /> {count || null}</h2>
-
-      <button onClick={ () => setHasLiked(!hasLiked) } > 
-          
-        {hasLiked ? '❤️' : '🤍'} 
-
-      </button>  
-      
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-  
-
   return (
-  <div className="card-container">
-
-    <Card title = "Game of Thrones" />
-    <Card title = "Breaking Bad"/>
-    <Card title = "Arrow" />
-    <Card title = "Pretty Woman"/>
-
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
