fc6a4d8d4d6d30d34fe1682cd1842b6db85678f8
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-04-12 02:17:23 +0200

    learned State and Hooks. Used usestate hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+
 const Card = ({ title  }) =>{
+const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={ () => setHasLiked(!hasLiked) } > 
+          
+        {hasLiked ? '❤️' : '🤍'} 
+
+      </button>  
+      
     </div>
   )
 }
 
 const App = () => {
+  
+
   return (
   <div className="card-container">
 
