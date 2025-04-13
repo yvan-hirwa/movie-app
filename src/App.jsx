8f5919e676496782f21207801348440d28893e9a
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-04-13 23:16:46 +0200

    header section(BG, hero img, Search)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import { use, useState } from "react"
+import Search from "./components/search"
 
 const App = () => {
+  const [searchTerm, setsearchTerm] = useState('');
+
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className='pattern' />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero-banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm ={searchTerm} setsearchTerm = {setsearchTerm} />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = ({searchTerm, setsearchTerm}) => {
+  return (
+    <div className='search'>
+        <div>
+            <img src="search.svg" alt="search" />
+
+            <input
+                type="text" 
+                placeholder="Search for movies"
+                value={searchTerm}
+                onChange={(e) => setsearchTerm(e.target.value)} 
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Search
